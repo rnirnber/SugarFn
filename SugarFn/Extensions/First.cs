20ade3c7433d728fab19e174a94a94b95bd64549
Raymond Nirnberger <rnirnber@gmail.com>
2014-08-02 12:43:09 -0700

    Adding first and last
    Added overloads for .First()<T> and .Last()<T> to support object
    reference equality via object.GetHashCode(). Useful for .ForEach()'ing
    and avoiding one-off cases for the first or last item.

---
+++ SugarFn/Extensions/First.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SugarFn.Extensions
+{
+    public static partial class _____SugarFnExtensions
+    {
+        public static bool First<T>(this ReadOnlyCollection<T> self, T item)
+        {
+            return item.GetHashCode() == self.First().GetHashCode();
+        }
+
+        public static bool First<T>(this List<T> self, T item)
+        {
+            return item.GetHashCode() == self.First().GetHashCode();
+        }
+    }
+}
+
+/*
+ * public static bool Last<T>(this ReadOnlyCollection<T> self, T item)
+        {
+            return item.GetHashCode() == self.Last().GetHashCode();
+        }
+
+        public static bool First<T>(this ReadOnlyCollection<T> self, T item)
+        {
+            return item.GetHashCode() == self.First().GetHashCode();
+        }*/
---
+++ SugarFn/Extensions/Last.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SugarFn.Extensions
+{
+    public static partial class _____SugarFnExtensions
+    {
+        public static bool Last<T>(this ReadOnlyCollection<T> self, T item)
+        {
+            return item.GetHashCode() == self.Last().GetHashCode();
+        }
+
+        public static bool Last<T>(this List<T> self, T item)
+        {
+            return item.GetHashCode() == self.Last().GetHashCode();
+        }
+    }
+}
