cdc1310cfbf483b28f1c5c0492787498ec0b7e10
Raymond Nirnberger <rnirnber@gmail.com>
2024-10-01 18:34:51 -0700

    Commit 1

--- SugarFn/Program.cs
+++ SugarFn/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using SugarFn.Extensions;
@@ -10,7 +11,121 @@ namespace SugarFn
     {
         static void Main(string[] args)
         {
+            // Example of "AND", single parameter
+            var is_even = (Func<int, bool>) ((int x) => x % 2 == 0);
+            var is_even_and_is_divisible_by_ten = is_even.AND<int>((int x) => x % 10 == 0);
+
+            Console.WriteLine("========================");
+            Console.WriteLine("Single argument 'AND'ing");
+            Console.WriteLine("========================");
+            Console.WriteLine("Is 6 even?");
+            Console.WriteLine(is_even(6));
+
+            Console.WriteLine("is 5 even?");
+            Console.WriteLine(is_even(5));
+
+            Console.WriteLine("Is thirty evenly divisible by 10?");
+            Console.WriteLine(is_even_and_is_divisible_by_ten(30));
+
+            Console.WriteLine("Is 45 evenly divisible by 10?");
+            Console.WriteLine(is_even_and_is_divisible_by_ten(45));
+
+            var is_left_parameter_greater_than_right_parameter = ((Func<int, int, bool>)((x, y) => x > y));
+            var is_left_parameter_more_than_double_left_parameter = is_left_parameter_greater_than_right_parameter.AND<int, int>((Func<int, int, bool>)((x, y) => (x / 2) > y));
+
+            Console.WriteLine("\n\n");
+
+            // Example of "AND", multiple parameters
+            Console.WriteLine("================");
+            Console.WriteLine("Multiple argument 'AND'ing");
+            Console.WriteLine("================");
+
+            Console.WriteLine("Is 5 greater than 2?");
+            Console.WriteLine(is_left_parameter_greater_than_right_parameter(5, 2));
+            Console.WriteLine("Is 5 greater than 10?");
+            Console.WriteLine(is_left_parameter_greater_than_right_parameter(5, 10));
+
+            Console.WriteLine("Is 25 more than double of 10?");
+            Console.WriteLine(is_left_parameter_more_than_double_left_parameter(25, 10));
+            Console.WriteLine("Is 30 more than double of 20");
+            Console.WriteLine(is_left_parameter_more_than_double_left_parameter(30, 20));
             
+            // Example of single parameter 'OR'ing
+            Console.WriteLine("==============");
+            Console.WriteLine("Single argument 'OR'ing");
+            Console.WriteLine("=============");
+
+            var is_even2 = (Func<int, bool>)((int x) => x % 2 == 0);
+            var is_even_or_evenly_divisible_by_three = is_even.OR<int>((int a) => a % 3 == 0);
+
+            for(int i = 1; i < 11; i++)
+            {
+                Console.WriteLine("Is " + i.ToString() + " even or evenly divisible by 3?");
+                Console.WriteLine(is_even_or_evenly_divisible_by_three(i));
+            }
+            Console.WriteLine("\n\n");
+
+            // Example of multiple parameter 'OR'ing
+            Console.WriteLine("===============");
+            Console.WriteLine("Multiple parameter 'OR'ing");
+            Console.WriteLine("===============");
+
+            var is_left_parameter_greater_than_right_parameter_v2 = (Func<int, int, bool>)((int x, int y) => x > y);
+            var is_left_parameter_greater_than_right_parameter_or_right_parameter_is_100 = is_left_parameter_greater_than_right_parameter_v2.OR<int, int>((x, y) => y == 100);
+
+            Console.WriteLine("Is 20 greater than 10?");
+            Console.WriteLine(is_left_parameter_greater_than_right_parameter_v2(20, 10));
+            Console.WriteLine("Is 10 greater than 20?");
+            Console.WriteLine(is_left_parameter_greater_than_right_parameter_v2(10, 20));
+            Console.WriteLine("Is 75 greater than 1, or is 1 100?");
+            Console.WriteLine(is_left_parameter_greater_than_right_parameter_or_right_parameter_is_100(75, 1));
+            Console.WriteLine("Is 0 greater than 100, or is 100 100?");
+            Console.WriteLine(is_left_parameter_greater_than_right_parameter_or_right_parameter_is_100(0, 100));
+
+            // "NOT'ing" examples...
+            
+            Console.WriteLine("\n\n");
+            Console.WriteLine("================");
+            Console.WriteLine("'NOT'ing w/1 parameter...");
+            Console.WriteLine("================");
+
+            var is_name_raymond = (Func<string, bool>)((str) => str == "Raymond");
+            var is_name_not_raymond = is_name_raymond.NOT<string>();
+
+            Console.WriteLine("Is Adam Raymond?");
+            Console.WriteLine(is_name_raymond("Adam"));
+            Console.WriteLine("Is Raymond Raymond?");
+            Console.WriteLine(is_name_raymond("Raymond"));
+
+            Console.WriteLine("Is Raymond not Raymond?");
+            Console.WriteLine(is_name_not_raymond("Raymond"));
+            Console.WriteLine("Is Adam not Raymond?");
+            Console.WriteLine(is_name_not_raymond("Adam"));
+
+            Console.WriteLine("\n\n");
+            Console.WriteLine("================");
+            Console.WriteLine("'NOT'ing w/multiple parameters...");
+            Console.WriteLine("================");
+
+            var names_list_one = new List<string>() { "Adam", "Bob", "Raymond" };
+            var names_list_two = new List<string>() { "Fabian", "Richard", "Peter" };
+            var is_raymond_in_list = (Func<string, List<string>, bool>)((name, n_list) => n_list.Contains("Raymond"));
+            var is_raymond_not_in_list = is_raymond_in_list.NOT();
+            var is_peter_in_list = (Func<string, List<string>, bool>)((name, n_list) => n_list.Contains("Peter"));
+            var is_peter_not_in_list = is_peter_in_list.NOT();
+
+            Console.WriteLine("Is Raymond in List 1");
+            Console.WriteLine(is_raymond_in_list("Raymond", names_list_one));
+            Console.WriteLine("Is Peter in List 1?");
+            Console.WriteLine(is_peter_in_list("Peter", names_list_one));
+            Console.WriteLine("Is Peter not in list 2?");
+            Console.WriteLine(is_peter_not_in_list("Peter", names_list_two));
+            Console.WriteLine("Is Raymond not in list 2?");
+            Console.WriteLine(is_raymond_not_in_list("Raymond", names_list_two));
+
+
+            Console.WriteLine("Press any key to exit");
+            var ignore_this = Console.ReadLine();
         }
     }
     
